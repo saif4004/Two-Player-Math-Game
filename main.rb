5cf1a8b16ea6dc4f0c07952f88cbf750dc31ee6a
Saif Al-saedi <saif4004@gmail.com>
2024-11-11 11:46:25 -0800

    Refactored it to create players, assign them to the game, and start the game

--- main.rb
+++ main.rb
@@ -1,26 +1,10 @@
 require_relative "Player"
-require_relative "Question"
+require_relative "Game"
+
 
 player1 = Player.new('Mike')
 player2 = Player.new('Adam')
 
-# puts player1
-# puts player2
 
-# player1.lose_life
-# puts player1
-# puts player1.is_alive?
-# puts player1
-
-# player1.lose_life
-# puts player1.is_alive?
-# puts player1
-# player1.update_score
-# puts player1.is_alive?
-# puts player1
-
-question1 = Question.new
-puts question1.genrate_question
-puts "Enter a your answer:  "
-answer = gets.chomp.to_i
-puts question1.check_answer(answer)
+game1 = Game.new(player1,player2)
+game1.start_game
