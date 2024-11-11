36059bbbf373f8a8db3600b73797e17c94a384c9
Saif Al-saedi <saif4004@gmail.com>
2024-11-11 11:41:31 -0800

    Fixed a spelling mistake 'genrate_question' to 'generate_question'

--- Question.rb
+++ Question.rb
@@ -5,17 +5,13 @@ class Question
     @q2 = rand(1..20)
     @answer = @q1 + @q2
   end
-  def genrate_question
+  def generate_question
     "what is #{@q1} + #{@q2}?"
   end
   def check_answer(player_answer)
-    if player_answer == @answer
-      "Yes, you are correct!"
-    else
-      "Seriously? NO!"
-    end
+    player_answer == @answer
   end
   def to_s 
-   "Question: #{@q1} + #{@q2} = ?"
+   "What does #{@q1} plus #{@q2} equal ?"
   end
 end
